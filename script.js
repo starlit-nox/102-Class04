a36ed0c6c977725a8ed796022afdb689c7431038
starlit-nox <kdkelly.1998@gmail.com>
2023-02-09 09:46:47 -0600

    Updated CSS & Added script.js
    Added CSS, images, and added script.js

---
+++ script.js
@@ -0,0 +1,30 @@
+let apple = 1;
+let myname = '';
+
+function applepie() {
+    let ingredientx = 5;
+    console.log(apple);
+}
+
+let pie2 = function(cinnamon) {
+    console.log(apple + cinnamon);
+}
+
+function whodat() {
+    myname = prompt("What is your name?");
+    if (myname != 'KD') {
+        alert('ERROR HACKER ERROR');
+    } else {
+        alert("Hello, " + myname)
+    } 
+    
+}
+
+// applepie();
+
+// pie2(9)
+
+// console.log(ingredientx);
+
+whodat();
+
