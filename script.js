e979a29e89ce270ff58db371af2082fd7d00d48e
starlit-nox <kdkelly.1998@gmail.com>
2023-02-09 11:04:07 -0600

    Login Button Added

--- script.js
+++ script.js
@@ -1,30 +1,3 @@
-let apple = 1;
-let myname = '';
-
-function applepie() {
-    let ingredientx = 5;
-    console.log(apple);
-}
-
-let pie2 = function(cinnamon) {
-    console.log(apple + cinnamon);
-}
-
-function whodat() {
-    myname = prompt("What is your name?");
-    if (myname != 'KD') {
-        alert('ERROR HACKER ERROR');
-    } else {
-        alert("Hello, " + myname)
-    } 
-    
-}
-
-// applepie();
-
-// pie2(9)
-
-// console.log(ingredientx);
-
-whodat();
-
+function login(){
+    let amount = prompt ("Enter Username");
+  
