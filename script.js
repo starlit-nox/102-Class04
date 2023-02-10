07cae7df4091b9fb0873d31106493b4574b4e11e
starlit-nox <kdkelly.1998@gmail.com>
2023-02-10 11:13:47 -0600

    Update script.js

--- script.js
+++ script.js
@@ -3,10 +3,6 @@ function userLogin() {
   location.replace("https://crazyballadventure.github.io/mock-website/")
 }
 
-  function signup() {
-    confirm("Sign ups aren't open right now. Check again later.")
-  }
-
 function Home() {
   confirm("You're already at home.")
 }
@@ -22,3 +18,31 @@ function People() {
   function Contact() {
     confirm("Please don't.")
   }
+
+  function signup(){
+    window.location="signup.html";
+}
+
+function signUp() {
+  if (document.getElementById("password2").value == document.getElementById("cfmpassword2").value) {
+      var users = new Object();
+      users.firstName = document.getElementById("firstName").value;
+      users.lastName = document.getElementById("lastName").value;
+      users.username2 = document.getElementById("username2").value;
+      users.email = document.getElementById("email").value;
+      users.password2 = document.getElementById("password2").value;
+
+
+      var postUser = new XMLHttpRequest(); // new HttpRequest instance to send user details
+
+      postUser.open("POST", "/users", true); //Use the HTTP POST method to send data to server
+
+      postUser.setRequestHeader("Content-Type", "application/json");
+
+      // Convert the data in "users" object to JSON format before sending to the server.
+      postUser.send(JSON.stringify(users));
+  }
+  else {
+      alert("Password column and Confirm Password column doesn't match!")
+  }
+}
